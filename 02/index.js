30d350e6722512cfc9aa058a9f8453ffb00130a0
Breno Aguiar <breno.boaventura321@gmail.com>
2022-11-28 22:57:21 -0300

    Exercicio 02 b)

---
+++ 02/index.js
@@ -0,0 +1,14 @@
+const carro={
+    ligado: false,
+    velocidade: 0,
+    ligar:function(){
+        if (this.ligado){
+            console.log("Este carro já está ligado.");
+        }else{
+            this.ligado=true;
+            console.log("Ligando");
+        }
+    },
+}
+
+carro.ligar();
